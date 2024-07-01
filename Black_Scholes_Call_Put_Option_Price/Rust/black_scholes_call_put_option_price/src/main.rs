eb8877f2409344bfb6fb17195f1219a716b09708
geekarim <karimalkhadzh@gmail.com>
2024-07-01 16:17:39 +0200

    Adding Rust 3

--- Black_Scholes_Call_Put_Option_Price/Rust/black_scholes_call_put_option_price/src/main.rs
+++ Black_Scholes_Call_Put_Option_Price/Rust/black_scholes_call_put_option_price/src/main.rs
@@ -21,26 +21,32 @@ fn black_scholes_call_put_option_price(
     sigma: f64  // Volatility
 ) -> (f64, f64) {
 
-    // Calculate d1 and d2
-    let d1 = (f64::ln(s / k) + (r + 0.5 * sigma * sigma) * t) / (sigma * f64::sqrt(t));
-    let d2 = d1 - sigma * f64::sqrt(t);
+    if t == 0.0 {
+        let call = f64::max(s - k,0.0);
+        let put = f64::max(k - s,0.0);
+        // Return
+        (call, put)
+    } else {
+        // Calculate d1 and d2
+        let d1 = (f64::ln(s / k) + (r + 0.5 * sigma * sigma) * t) / (sigma * f64::sqrt(t));
+        let d2 = d1 - sigma * f64::sqrt(t);
 
-    // Create a standard normal distribution
-    let normal = Normal::new(0.0, 1.0).unwrap();
+        // Create a standard normal distribution
+        let normal = Normal::new(0.0, 1.0).unwrap();
 
-    // Calculate the call & put option prices
-    let call = s * normal.cdf(d1) - k * E.powf(-r * t) * normal.cdf(d2);
-    let put = k * E.powf(-r * t) * normal.cdf(-d2) - s * normal.cdf(-d1);
-
-    // Return
-    (call, put)
+        // Calculate the call & put option prices
+        let call = s * normal.cdf(d1) - k * E.powf(-r * t) * normal.cdf(d2);
+        let put = k * E.powf(-r * t) * normal.cdf(-d2) - s * normal.cdf(-d1);
+        // Return
+        (call, put)
+    }
 }
 
 fn main() {
     let s = 100.0;      // Stock Price
     let k = 100.0;      // Strike
-    let r = 0.05;       // Interest Rate
-    let t = 1.0;        // Time to maturity
+    let r = 0.02;       // Interest Rate
+    let t = 0.3;        // Time to maturity
     let sigma = 0.2;    // Volatility
 
     let (call_price, put_price) = black_scholes_call_put_option_price(s, k, r, t, sigma);
